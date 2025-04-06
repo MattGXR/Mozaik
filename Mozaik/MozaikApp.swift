1148121c0796e9aca24528028be16707f27ed48a
Matt <60483021+MattGXR@users.noreply.github.com>
2025-04-06 09:41:16 +0200

    Initial Commit

---
+++ Mozaik/MozaikApp.swift
@@ -0,0 +1,17 @@
+//
+//  MozaikApp.swift
+//  Mozaik
+//
+//  Created by Mattia Meligeni on 06/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MozaikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
